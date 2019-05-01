a843c41d3a9eba770d064db21b4a4adecf8f8079
Pulkit Chandra <pchandra@pivotal.io>
2019-05-01 14:57:30 -0700

    Actually using the service bean

--- src/main/java/io/pivotal/pcc/example/demo/config/HelloWorldController.java
+++ src/main/java/io/pivotal/pcc/example/demo/config/HelloWorldController.java
@@ -3,6 +3,7 @@ package io.pivotal.pcc.example.demo.config;
 import io.pivotal.pcc.example.demo.service.HelloWorldService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,12 @@ public class HelloWorldController {
 
   private static Logger logger = LogManager.getLogger(HelloWorldController.class);
 
+  @Autowired
+  HelloWorldService helloWorldService;
+
   @RequestMapping("/hello")
   public String sayHello(){
     logger.info("**Received request**");
-    HelloWorldService helloWorldService = new HelloWorldService();
-
     return helloWorldService.sayHelloWorld("HelloWorld");
   }
 
