e995372f83de56e30d2853dcd466d16f5a137160
pnikonowicz <paul.nikonowicz@gmail.com>
2019-08-02 15:15:48 -0400

    add cors headers
    this is so that the home service can consume this service

--- pcc-hello-world-example/src/main/java/io/pivotal/pcc/example/demo/config/FrontEndController.java
+++ pcc-hello-world-example/src/main/java/io/pivotal/pcc/example/demo/config/FrontEndController.java
@@ -22,6 +22,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
@@ -39,6 +40,7 @@ public class FrontEndController {
     this.helloWorldService = helloWorldService;
   }
 
+  @CrossOrigin(origins = "*", allowedHeaders = "*")
   @GetMapping("/")
   public String ping(Model model, HttpSession httpSession){
     if ((httpSession.getAttribute("name")) !=null || httpSession.getAttribute("message") != null){
